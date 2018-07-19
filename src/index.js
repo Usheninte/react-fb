13a8cc6afc30ff3673ee6a4c59f2fa8c9b65538b
Usheninte <jrandangana@gmail.com>
2018-07-19 14:17:09 +0100

    Greatness lies before me

--- src/index.js
+++ src/index.js
@@ -24,6 +24,12 @@ class Board extends React.Component {
     };
   }
 
+  handleClick() {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
     return (
       <Square 
