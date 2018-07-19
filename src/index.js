38fa5d229d4f3abfe62284028eaa336248b8bf2d
Usheninte <jrandangana@gmail.com>
2018-07-19 19:59:10 +0100

    Greatness will not be easy. Embrace it still.

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if (calculateWinner(square) || squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O' ;
